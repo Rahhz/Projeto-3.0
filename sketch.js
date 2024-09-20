46973f9969bb1e3028f705473a19ed5979cc9c0c
Raíssa <00001132653551sp@al.educacao.sp.gov.br>
2024-09-20 10:00:54 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(550, 500);
+    background("#FFA0C3");
+  }
+  
+  function draw() {
+    stroke ("rgb(248,243,248)"); 
+    fill ("rgb(252,206,252)");
+    
+    // console.log (mouseIsPressed);
+    
+    if (mouseIsPressed) {
+     } rect (mouseX, mouseY, 30, 50);
+    }
